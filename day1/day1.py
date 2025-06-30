9077e95ebca5ce097cf80731942bdedfc19057ac
dipika-vaishnav <dipika.github@gmail.com>
2025-06-30 16:57:29 +0530

    first-commit

---
+++ day1/day1.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+print("Hello,",name,"Welcome! to Python learning")
+age=int(input("What is your age: "))
+future_age = age + 5
+print("In 5 yeras you will be",future_age,"years old")
